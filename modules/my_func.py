1564d0e3ecf5d1d2a3e09ebe881e45743f163be0
gittil <douglas.littig@gmail.com>
2022-03-23 19:16:41 -0300

    commit

---
+++ modules/my_func.py
@@ -0,0 +1,17 @@
+
+
+
+def converterColuna(dataframe, nome_col, novoTipo):
+    """Função para converter uma coluna no DF
+
+    Args:
+        dataframe (dataframe): passamos nesse parâmetro o nome do DF que será alterado a coluna 
+        nome_col (nome da coluna): passamos nesse parâmetro qual a coluna que do DF que irá receber a alterção
+        novoTipo (novo tipo): passamos nesse parâmetro qual o novo tipo que a coluna irá receber
+
+    Returns:
+        DF: retorna o DF com as colunas alteradas
+    """
+    for nome in nome_col:
+        dataframe = dataframe.withColumn(nome,dataframe[nome].cast(novoTipo))
+    return dataframe 
